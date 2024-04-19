e1f510dfc9fd10e950ccccc5eccbe612fe003d46
Daniele De Lise <danieledelise12@gmail.com>
2024-04-19 18:33:11 +0200

    correzione eser

--- react-route/src/App.jsx
+++ react-route/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Users from "./pages/Users";
+import { Routes, Route, Link } from "react-router-dom";
+import GithubUserList from "./pages/GithubUserList";
 import Homepage from "./pages/Homepage";
 import GithubUser from "./components/GithubUser";
 import Counter from "./components/Counter";
@@ -11,12 +11,26 @@ function App() {
     <div>
       <Routes>
         <Route path="/counter" element={<Counter />} />
-        <Route path="/users" element={<Users />}>
+        <Route path="/users" element={<GithubUserList />}>
+          <Route index element={"Enter a user or select it!"}></Route>
           <Route path=":username" element={<GithubUser />} />
         </Route>
         <Route path="/" element={<Homepage />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
+      <nav>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/users">
+            <li>Users</li>
+          </Link>
+          <Link to="/counter">
+            <li>Counter</li>
+          </Link>
+        </ul>
+      </nav>
     </div>
   );
 }
--- react-route/src/pages/Homepage.jsx
+++ react-route/src/pages/Homepage.jsx
@@ -1,21 +1,9 @@
 import "/src/App.css";
-import { Link } from "react-router-dom";
+
 
 function Homepage() {
   return (
-    <nav>
-      <ul>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/users">
-        <li>Users</li>
-        </Link>
-        <Link to="/counter">
-        <li>Counter</li>
-        </Link>
-      </ul>
-    </nav>
+    <h1>Homepage</h1>
   );
 }
 
