a3f87b2b3a9abb64b26e0838557b944031b21f36
Daniele De Lise <danieledelise12@gmail.com>
2024-05-16 20:41:54 +0200

    async await  done

---
+++ eser_node/async.js
@@ -0,0 +1,31 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+  
+  async function getResults() {
+    const results = await Promise.allSettled([
+      luckyDraw('Tina'),
+      luckyDraw('Jorge'),
+      luckyDraw('Julian')
+    ]);
+  
+    results.forEach(result => {
+      if (result.status === 'fulfilled') {
+        console.log(result.value);
+      } else {
+        console.error(result.reason);
+      }
+    });
+  }
+  
+  getResults();
