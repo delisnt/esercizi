cfffed3b6850d2e96f7fa329d0c1e8fc3785107b
Daniele De Lise <danieledelise12@gmail.com>
2024-05-16 19:27:10 +0200

    promise done

---
+++ eser_node/promise.js
@@ -0,0 +1,29 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+luckyDraw("Joe")
+  .then((player) => {
+    console.log(player);
+  })
+  .then(() => luckyDraw("Caroline"))
+  .then((player) => {
+    console.log(player);
+  })
+  .then(() => luckyDraw("Sabrina"))
+  .then((player) => {
+    console.log(player);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
