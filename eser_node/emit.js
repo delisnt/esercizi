b94b7101b6776359b5340612bc6563a3cf9c584c
Daniele De Lise <danieledelise12@gmail.com>
2024-05-17 09:05:07 +0200

    emit done

---
+++ eser_node/emit.js
@@ -0,0 +1,31 @@
+const { EventEmitter } = require("node:events");
+
+function createNewsFeed() {
+  const emitter = new EventEmitter();
+
+  emitter.on('newsEvent', (message) => {
+    console.log(message)
+  })
+
+  emitter.on('breakingNews', (message) => {
+    console.log(message)
+  })
+
+
+
+  setInterval(() => {
+    emitter.emit("newsEvent", "News: A thing happened in a place.");
+  }, 1000);
+
+  setInterval(() => {
+    emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+  }, 4000);
+
+  setTimeout(() => {
+    emitter.emit("error", new Error("News feed connection error"));
+  }, 5000);
+
+  return emitter;
+}
+
+const newsFeed = createNewsFeed();
