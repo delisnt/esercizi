e8ac6076ff00e4ca481a5b496ad68e9b9fc6fef6
Daniele De Lise <129328128+delisnt@users.noreply.github.com>
2024-06-09 01:12:37 +0200

    Add files via upload

--- eser_node/server.ts
+++ eser_node/server.ts
@@ -1,16 +1,30 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
+import multer from "multer";
 import {
   getAll,
   getOneById,
   create,
   updateById,
   deleteById,
-} from "./controller";
+  createImage,
+} from "./controller/planets.js";
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 const app = express();
-const port = 4000;
+const port = 5432;
+
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -21,10 +35,12 @@ app.get("/api/planets/:id", getOneById);
 
 app.post("/api/planets", create);
 
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+
 app.put("/api/planets/:id", updateById);
 
 app.delete("/api/planets/:id", deleteById);
 
 app.listen(port, () => {
-  console.log("My server, listening to http://localhost:4000/api/planets");
+  console.log("My server, listening to http://localhost:5432/api/planets");
 });
