5b4c6f4ca4091f9abae0b4980d5748a4ea8abc36
Daniele De Lise <129328128+delisnt@users.noreply.github.com>
2024-06-08 16:47:27 +0200

    Add files via upload

---
+++ eser_node/eser-db/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+} from "./controller";
+
+const app = express();
+const port = 4000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/api/planets", getAll);
+
+app.get("/api/planets/:id", getOneById);
+
+app.post("/api/planets", create);
+
+app.put("/api/planets/:id", updateById);
+
+app.delete("/api/planets/:id", deleteById);
+
+app.listen(port, () => {
+  console.log("My server, listening to http://localhost:4000/api/planets");
+});
