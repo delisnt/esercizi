af8416cf52d118bb8532bdfdd03c6db850c9d5d0
Daniele De Lise <danieledelise12@gmail.com>
2024-06-09 16:52:48 +0200

    eser 3

---
+++ eser_node/eser-jwt3/dh-nodejs-part2-starter-kit/src/server.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import multer from "multer";
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+  createImage,
+} from "./controller/planets.js";
+import {
+  logIn,
+  signUp,
+  getAllUsers,
+  getUser,
+  logOut,
+} from "./controller/users.js";
+import authorize from "./authorize.js";
+import "./passport.js";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+const app = express();
+const port = 3000;
+
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/api/planets", getAll);
+app.get("/api/planets/:id", getOneById);
+
+app.get("/api/users", getAllUsers);
+app.get("/api/users/:id", getUser);
+
+app.post("/api/planets", create);
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+app.post("/api/users/login", logIn);
+app.post("/api/users/signup", signUp);
+app.post("/api/users/logout", authorize, logOut);
+
+app.put("/api/planets/:id", updateById);
+
+app.delete("/api/planets/:id", deleteById);
+
+app.listen(port, () => {
+  console.log("My server, listening to http://localhost:3000/api/planets");
+});
